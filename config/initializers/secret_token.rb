379b2825704aa7cb7c2e448c16167a99958346cb
Jared Clement <jclement@cs.washington.edu>
2012-11-17 10:16:12 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TonightsPlaylistApp::Application.config.secret_token = '6017c83697b62fd1acd084f107b2bc660f1bb9b0cfc761a3de90a2eaca22928e6bce8ae3fbc194cffd6cabf276c2c09d8bba06fc15aec5b6efe0bb59f1ee82bd'
