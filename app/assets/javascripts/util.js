0c3ec1273f8b7c10241d7778e4874f2336cdf546
Shahaf Abileah <shahaf.abileah@shahafmac.rf.lan>
2012-11-18 11:46:33 -0800

    Using the *right* styles for the playback controls

---
+++ app/assets/javascripts/util.js
@@ -0,0 +1,23 @@
+/*
+
+Utility methods.
+
+TODO: replace this with jQuery or something.
+
+*/
+
+function hasClass(el, name) {
+   return new RegExp('(\\s|^)'+name+'(\\s|$)').test(el.className);
+}
+
+function addClass(el, name)
+{
+   if (!hasClass(el, name)) { el.className += (el.className ? ' ' : '') +name; }
+}
+
+function removeClass(el, name)
+{
+   if (hasClass(el, name)) {
+      el.className=el.className.replace(new RegExp('(\\s|^)'+name+'(\\s|$)'),' ').replace(/^\s+|\s+$/g, '');
+   }
+}
