11bfd9f5e37187cd0e09cf558a2763d20e07c072
Jared Clement <jclement@cs.washington.edu>
2012-11-18 15:37:08 -0800

    syntax

--- app/assets/javascripts/tracklist.js
+++ app/assets/javascripts/tracklist.js
@@ -93,7 +93,6 @@ function createTracklistController(tracks) {
 	// Each track controller keeps a reference to the tracklistController (this) 
 	// so it can percolate up events (e.g. "my play button was clicked")
 	var trackControllers = [];
-	debugger;
 	for (var i = 0; i < tracks.length; i++) {
 		var trackData = tracks[i];
 
